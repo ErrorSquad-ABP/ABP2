1da64cf5591182d8bda268ebb69f0e9c647fe24d
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-09-22 20:56:38 -0300

    Adding route to get reservatorio by id

--- server/src/routes/furnas/reservatorio.routes.ts
+++ server/src/routes/furnas/reservatorio.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getAll } from "../../controllers/furnas/reservatorio.controller";
+import { getAll, getByPage, getById } from "../../controllers/furnas/reservatorio.controller";
 
 const router = Router();
 
 router.get("/all", getAll);
+
+// GET instituição específica por ID
+router.get("/id/:id", getById);
 
 export default router;
