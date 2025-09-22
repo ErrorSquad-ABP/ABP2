98ed2bc64b59ede5240503cd8c7d1205a9c006d0
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-09-22 20:54:21 -0300

    Altering reservatorio route to return all the rows

--- server/src/controllers/furnas/reservatorio.controller.ts
+++ server/src/controllers/furnas/reservatorio.controller.ts
@@ -6,10 +6,6 @@ const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 10;
 
 export const getAll = async (req: Request, res: Response): Promise<void> => {
   try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || PAGE_SIZE;
-    const offset = (page - 1) * limit;
-
     // consulta com paginação
     const result = await furnasPool.query(
       `
@@ -19,9 +15,7 @@ export const getAll = async (req: Request, res: Response): Promise<void> => {
         lat,
         lng
       FROM tbreservatorio
-      LIMIT $1 OFFSET $2
-      `,
-      [limit, offset],
+      ` 
     );
 
     // consulta total de registros
@@ -30,10 +24,7 @@ export const getAll = async (req: Request, res: Response): Promise<void> => {
 
     res.status(200).json({
       success: true,
-      page,
-      limit,
       total,
-      totalPages: Math.ceil(total / limit),
       data: result.rows, 
     });
   } catch (error: any) {
