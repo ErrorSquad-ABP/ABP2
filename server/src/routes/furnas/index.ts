d8fd0b9a6dd4b8c75dd8c60aa02943f1fee45e09
AlineRaquelC <aline.oliveira105@fatec.sp.gov.br>
2025-09-22 21:59:41 -0300

    BDR 006 endpoint

--- server/src/routes/furnas/index.ts
+++ server/src/routes/furnas/index.ts
@@ -4,8 +4,13 @@ import campanha from "./campanha.routes";
 import instituicao from "./instituicao.routes";
 import reservatorio from "./reservatorio.routes";
 import sitio from "./sitio.routes";
+import dataRoutes from "./data.routes";
 
 const router = express.Router();
+
+
+router.use("/", dataRoutes);
+
 
 router.use("/abioticocoluna", abioticocoluna);
 router.use("/campanha", campanha);
