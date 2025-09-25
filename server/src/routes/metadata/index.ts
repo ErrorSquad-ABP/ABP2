64468dff9b99cf1a8138b1cfd89119e46976d177
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-09-24 21:03:19 -0300

    Adding metadata router prototype

---
+++ server/src/routes/metadata/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import metadata from "./metadata.routes";
+
+const router = express.Router();
+
+/* router.use("/sima", sima);
+router.use("/simaoffline", simaoffline); */
+
+export default router;
