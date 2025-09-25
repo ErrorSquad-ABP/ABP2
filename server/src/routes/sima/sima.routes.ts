e9566eb9d75aafef77b76e0a35975fe6a93829e8
Carlos-Santo <ceges121106@gmail.com>
2025-09-25 14:47:54 -0300

    Foi criada a função recursiva para ordenar por id

--- server/src/routes/sima/sima.routes.ts
+++ server/src/routes/sima/sima.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAll } from "../../controllers/sima/sima.controller";
+import { getAll, getAllSortedById } from "../../controllers/sima/sima.controller";
 
 const router = Router();
 
 router.get("/all", getAll);
+
+router.get("/allSorted", getAllSortedById);
 
 export default router;
