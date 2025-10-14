746b0a6422f4e84613f0a2b3613785d6ae0d44fe
Carlos-Santo <ceges121106@gmail.com>
2025-10-14 19:49:55 -0300

    feature/balcar-campanha-bd079 Foi criado o endpoint para as campanhas do balcar

--- server/src/routes/balcar/index.ts
+++ server/src/routes/balcar/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import fluxoinpe from "./fluxoinpe.routes";
 import reservatorio from "./reservatorio.routes";
+import { getCampaigns } from "../../controllers/balcar/campanha.controller";
 
 const router = express.Router();
 
 router.use("/fluxoinpe", fluxoinpe);
 router.use("/reservatorio", reservatorio);
+router.get("/campanha", getCampaigns);
 
 export default router;
