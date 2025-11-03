be2e1c5bb0d6a6a5efe0818c615c2eae64ff3cb3
AlineRaquelC <aline.oliveira105@fatec.sp.gov.br>
2025-11-03 11:58:40 -0300

    feature/criarendpoint0077BD

--- server/src/routes/furnas/data.routes.ts
+++ server/src/routes/furnas/data.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getData } from "../../controllers/furnas/data.controller";
 import { getReservatorioById } from "../../controllers/furnas/dataByReservatorio.controller";
+import { getDataByInstituicao } from "../../controllers/furnas/getDataByInstituicao";
 console.log("getReservatorioById:", getReservatorioById); // deve aparecer: [AsyncFunction: getReservatorioById]
 
 const router = Router();
@@ -8,5 +9,7 @@ const router = Router();
 router.get("/:table/data", getData);
 
 router.get("/reservatorio/:id", getReservatorioById);
+
+router.get("/:table/data/by-instituicao", getDataByInstituicao);
 
 export default router;
