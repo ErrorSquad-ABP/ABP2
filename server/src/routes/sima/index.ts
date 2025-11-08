20fdaf2befe7d7ca6adf4493c819f07d82c7bc26
JV-L0pes <joaovlr9@gmail.com>
2025-11-08 20:07:15 -0300

    deploy 4

--- server/src/routes/sima/index.ts
+++ server/src/routes/sima/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import sima from "./sima.routes";
 import simaoffline from "./simaoffline.routes";
+import { getSimaInfo } from "../../controllers/sima/index.controller";
 
 const router = express.Router();
+
+// Rota raiz para /sima
+router.get("/", getSimaInfo);
 
 router.use("/sima", sima);
 router.use("/simaoffline", simaoffline);
