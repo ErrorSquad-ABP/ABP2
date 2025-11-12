52f32476c1ac97960a6420a3d94d495687963667
AlineRaquelC <aline.oliveira105@fatec.sp.gov.br>
2025-11-11 21:07:36 -0300

    feature/rotaparapegarasdatastabeladatas0090BD

--- server/src/routes/data.routes.ts
+++ server/src/routes/data.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getDataByInstituicao } from "../controllers/furnas/getDataByInstituicao";
+import { getDatesFromTable } from "../controllers/dataByDate.controller";
 
 const router = Router();
 
 // Endpoint atalho para consulta por instituição
 router.get("/tablesbyinst/:table/data/by-instituicao", getDataByInstituicao);
+router.get("/:table/datas", getDatesFromTable);
 
 export default router;
