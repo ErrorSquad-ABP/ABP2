11f9ff46d9c85dc4962b1e7c1713b199c6988360
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-11-24 21:39:30 -0300

    Adicionado try/catch e status 500 caso haja erro na requisição - bd096

--- server/src/controllers/data/health.controller.ts
+++ server/src/controllers/data/health.controller.ts
@@ -1,13 +1,23 @@
 // server/src/controllers/data/health.controller.ts
 import { Request, Response } from "express";
+import { logger } from "../../configs/logger";
+
 
 export const health = async (req: Request, res: Response): Promise<void> => {
   try {
     res.status(200).json({
       success: "Rota funcionando!",
     });
-  } catch {
-    // Mantemos o catch para futuras operações que possam lançar erro.
-    // Se quiser logar futuramente, adicione: console.error(err);
+  } catch (error: any) {
+    logger.error("Erro ao consultar tbfluxoinpe", {
+      message: error.message,
+      stack: error.stack,
+    });
+
+    res.status(500).json({
+      success: false,
+      error: "Erro ao realizar a operação.",
+    });
   }
+
 };
